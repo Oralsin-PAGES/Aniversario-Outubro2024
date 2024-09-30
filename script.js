7c6f9ec0a70a7fb97a446207ec39b1699f123076
Oralsin-PAGES <143753787+Oralsin-PAGES@users.noreply.github.com>
2024-09-30 10:24:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const images = [
+        "Parabens.png",
+        "Parabens2.jpeg",
+        "02-10-Rotativo.jpeg",
+        "02-10.jpeg",
+        "04-10.jpeg",
+        "05-10.jpeg",
+        "09-10.jpeg",
+        "09-10-rotativo.jpeg",
+        "16-10.jpeg",
+        "21-10.jpeg",
+        "22-10.jpeg",
+        "23-10.Jpeg",
+        "23-10-vlm.jpeg",
+        "24-10.jpeg",
+        "26-10.jpeg",
+        "29-10.jpeg",
+        "29-10-Rotativo.jpeg",
+        "30-10.jpeg",
+        // Adicione mais imagens aqui
+    ];
+
+    let currentIndex = 0;
+    const centeredImage = document.querySelector(".centered-image");
+
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        const imageUrl = images[currentIndex];
+        centeredImage.src = imageUrl;
+    }
+
+    // Inicializa a troca de imagem a cada 5 segundos
+    setInterval(changeImage, 5000);
+
+    // Carrega a primeira imagem
+    changeImage();
+});
